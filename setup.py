74b9d23d3c940eedb48f6ce0f7f6dff6e1f068c8
Ivy <hp15071hp@gmail.com>
2014-07-28 20:26:03 +0800

    edit setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name = 'parallelization',
+    packages = ['parallelization'],
+    version = '0.1',
+    author='Chan-Chun Liu',
+    author_email = 'hp15071hp@gmail.com',
+    url = 'https://github.com/hp15071hp/Parallelization',
+    description = 'A parallel processing test library based on Robot Framework',
+    long_description = 
+    """
+    This is a test library for [Robot framework](https://pypi.python.org/pypi/robotframework) which provides parallel processing keyword function.
+    """,
+    classifiers  = [
+                    'Programming Language :: Python :: 2.7',
+                    'License :: OSI Approved :: MIT License',
+                    'Operating System :: Microsoft :: Windows :: Windows 7',
+                    'Development Status :: 3 - Alpha',
+                    'Intended Audience :: Developers',
+                    'Topic :: Software Development :: Testing'
+                    ]
+)
